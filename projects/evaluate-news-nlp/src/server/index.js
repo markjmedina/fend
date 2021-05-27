2a79f7b2d9edd21d5e2d1e190d3f1c16e0d46947
Mark Medina <markjmedina@Marks-MacBook-Air.local>
2021-05-27 17:21:46 +1000

    check if submission is ok

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(`The API key used is ${process.env.API_KEY}`);
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -5,6 +10,24 @@ const mockAPIResponse = require('./mockAPI.js')
 const app = express()
 
 app.use(express.static('dist'))
+
+const formdata = new FormData();
+formdata.append("key", process.env.API_KEY);
+formdata.append("lang", "EN");  // 2-letter code, like en es fr ...
+
+const requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+const response = fetch("https://api.meaningcloud.com/sentiment-2.1?key=process.env.API_KEY&of=json&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.&model=Restaurants&lang=en", requestOptions)
+  .then(response => ({
+    status: response.status, 
+    body: response.json()
+  }))
+  .then(({ status, body }) => console.log(status, body))
+  .catch(error => console.log('error', error));
 
 console.log(__dirname)
 
